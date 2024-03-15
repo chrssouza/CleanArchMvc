cfe166e2059ea49f765465e0ec6cc15a219ec8f4
Christian Souza <chrsouza012@gmail.com>
2024-03-15 11:44:58 -0300

    Configuração do Mediator, assembly, mapping e alteração no método GetByIdAsync

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Products.Handlers;
 using CleanArchMvc.Application.Services;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
@@ -7,6 +8,7 @@ using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CleanArchMvc.Infra.IoC
 {
@@ -14,8 +16,8 @@ namespace CleanArchMvc.Infra.IoC
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<ApplicationDbContext>(options => 
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), 
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
             x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
@@ -23,6 +25,17 @@ namespace CleanArchMvc.Infra.IoC
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            //var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            //services.AddMediatR(myHandlers);
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CleanArchMvc.Application.Products.Handlers.GetProductByIdQueryHandler).Assembly));
+                 //typeof(GetProductsQueryHandler).Assembly,
+                 //typeof(ProductCreateCommandHandler).Assembly,
+                 //typeof(ProductRemoveCommandHandler).Assembly, 
+                 //typeof(ProductUpdateCommandHandler).Assembly));
+
+
 
             return services;
         }
