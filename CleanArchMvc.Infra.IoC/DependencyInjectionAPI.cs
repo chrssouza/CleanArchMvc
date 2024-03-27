b931b7a4dc43cfccbe5df324c182d9d1ae1d595c
Christian Souza <chrsouza012@gmail.com>
2024-03-27 10:57:19 -0300

    Método de extensão para API e string de conexão.

---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                 .AddDefaultTokenProviders();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();  
+                
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CleanArchMvc.Application.Products.Handlers.GetProductByIdQueryHandler).Assembly));
+            //typeof(GetProductsQueryHandler).Assembly,
+            //typeof(ProductCreateCommandHandler).Assembly,
+            //typeof(ProductRemoveCommandHandler).Assembly, 
+            //typeof(ProductUpdateCommandHandler).Assembly));
+
+
+
+            return services;
+        }
+    }
+}
