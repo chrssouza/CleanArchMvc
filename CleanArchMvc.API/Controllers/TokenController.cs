a8546beb6fdf8ba04df12c911644d84f1fd1eb75
Christian Souza <chrsouza012@gmail.com>
2024-03-28 12:55:08 -0300

    Criado Endpoint CreateUser

--- CleanArchMvc.API/Controllers/TokenController.cs
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -22,6 +22,23 @@ namespace CleanArchMvc.API.Controllers
             _configuration = configuration;
         }
 
+        [HttpPost("CreateUser")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<ActionResult> CreateUser([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.RegisterUser(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {                
+                return Ok($"User {userInfo.Email} was created successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
         [HttpPost("LoginUser")]
         public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
         {
@@ -36,7 +53,6 @@ namespace CleanArchMvc.API.Controllers
                 ModelState.AddModelError(string.Empty, "Invalid Login Attempt.");
                 return BadRequest(ModelState);
             }
-
 
         }
 
